d137e91f6e13a37483e5edaff9c2fd353183c8be
lasse <77295879+lassejlv@users.noreply.github.com>
2025-04-09 14:26:25 +0200

    Add bearer auth and logging for upload page
    The change adds auth protection to /upload along with logging to show auth and
    upload page configuration status during startup.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import { Hono } from "hono";
 import UploadRouter from "./routes/upload";
 import FilesRouter from "./routes/files";
+import consola from "consola";
+import { bearerAuth } from "hono/bearer-auth";
 
 const app = new Hono();
 
 const DISABLE_UPLOAD_PAGE =
   process.env.FILE_SERVER_DISABLE_UPLOAD_PAGE === "true";
 
+const REQUIRE_AUTH = process.env.FILE_SERVER_AUTH_TOKEN;
+
+if (REQUIRE_AUTH) {
+  consola.success("Auth at /upload is enabled. This is recommended for prod.");
+  app.use("/upload", bearerAuth({ token: REQUIRE_AUTH }));
+} else {
+  consola.warn(
+    "Auth at /upload is disabled. This is not recommended for prod.",
+  );
+}
+
 if (!DISABLE_UPLOAD_PAGE) {
+  consola.warn(
+    "Running with upload page enabled, in prod you may wanna disable this. Set the env variable FILE_SERVER_DISABLE_UPLOAD_PAGE=true",
+  );
   app.get("/", async (c) => {
     const html = await Bun.file("upload.html").text();
     return c.html(html);
